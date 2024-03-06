2894fba6c2dc3e841081c4dd08cb6ac57f4eeacc
Aking <www.aking016@gmail.com>
2024-03-06 21:18:02 +0330

    SPA Initial Setup

---
+++ app.js
@@ -0,0 +1,31 @@
+function homeView() {
+    return '<h1>Home Page</h1>';
+}
+
+function aboutView() {
+    return '<h1>About Page</h1>';
+}
+
+function handleRouteChange() {
+    const path = window.location.pathname;
+    let view;
+
+    switch (path) {
+        case '/about':
+            view = aboutView();
+            break;
+
+        default:
+            view = homeView();
+    }
+
+    document.getElementById('app').innerHTML = view;
+}
+
+document.querySelectorAll('.route').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        history.pushState(null, '', this.href);
+        handleRouteChange();
+    });
+});
